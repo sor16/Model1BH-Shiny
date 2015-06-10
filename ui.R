9726c6da61a65588041e58c5ffcb11b78161de79
sor16 <solviro@gmail.com>
2015-06-10 15:08:10 +0000

    ui changes (country and name of river moved up) and changes to rmarkdown file

--- ui.R
+++ ui.R
@@ -4,17 +4,17 @@ shinyUI(fluidPage(
     titlePanel('Rating curve'),
     sidebarLayout(
         sidebarPanel(
-            fileInput('file1', 'Choose file'),
-            br(),
             selectInput("select", label = "Select country", choices = list("Iceland" = 'Iceland'), 
                         selected = 'Iceland'),
             br(),
+            textInput("name","Name of river"),
+            br(),
+            fileInput('file1', 'Choose file'),
             checkboxGroupInput("checkbox", label = "Output",choices=list("Raunskali"="raun","Lograskali"="log",
                                                                          "Leifarit a raunskala"="leifraun","Leifarit a logskala"="leiflog") ,selected = NULL),
             actionButton("go", label="Submit"),
             br(),
             br(),
-            textInput("name","Name of river"),
             downloadButton('downloadReport',label="Download as PDF")
             
         ),
