bfa452a1651d0e04d946e1781ca08b3832faa978
sor16 <solviro@gmail.com>
2015-06-23 14:10:44 +0000

    deletion of garbage i server

--- server.R
+++ server.R
@@ -45,146 +45,8 @@ shinyServer(function(input, output) {
         })
         
       ## MODEL1 ##  Begin
-      
-model1<-reactive({
-      if(!is.null(data())){
-          wq=data()$wq
-          qvdata=data()$qvdata
-          withProgress(message = 'Making plot', value = 0, {
-              RC=priors(input$select)
-              
-              RC$y=as.matrix(log(wq[,2]));
-              RC$w=0.01*wq[,1]; #to meters 
-              RC$w_tild=RC$w-min(RC$w);
-              RC$n=length(RC$y);
-              
-              
-              Dens <- function(th){ Densevalm11(th,RC)$pmin}
-              Densmin=optim(par=c(0,0),Dens,hessian=TRUE)
-              t_m=as.matrix(Densmin$par)
-              H=Densmin$hessian
-              
-        
-              l_m=as.matrix(log(RC$w_tild+exp(t_m[1,]))); 
-              
-              X_m=cbind(matrix(1,nrow(l_m),ncol(l_m)),l_m); 
-              
-              L=t(chol(RC$Sig_xinv+t(X_m)%*%X_m/exp(t_m[2,]))); 
-              
-              mu=solve(t(L),(solve(L,(RC$Sinvmu+t(X_m)%*%RC$y/exp(t_m[2,]))))); 
-              
-              RC$fit=X_m%*%mu
-              
-              
-              v_temp=X_m%*%solve(RC$Sig_xinv+t(X_m)%*%X_m/exp(t_m[2,]))%*%t(X_m) 
-              
-              
-              varappr=mean(as.matrix(diag(v_temp)+exp(t_m[2,]))) 
-              
-              
-              confinterval= cbind(X_m%*%mu+qnorm(0.025,0,sqrt(varappr)),X_m%*%mu+qnorm(0.975,0,sqrt(varappr))) 
-              
-              LH=t(chol(H))/(2.38/sqrt(2)) #Hvadan kemur thessi tala?? 2.38
-              
-              
-              Nit=20000
-              
-              t1=matrix(0,4,Nit)
-              t2=matrix(0,4,Nit)
-              t3=matrix(0,4,Nit)
-              t4=matrix(0,4,Nit)
-              
-              
-              
-              for(j in 1:4){
-                  incProgress(1/4, detail = paste("Doing part", j))
-                  
-                  t_old=t_m
-                  t=matrix(0,nrow=4,ncol=Nit)
-                  yp=matrix(0,nrow=nrow(wq),ncol=Nit)
-                  ypo=matrix(0,nrow=nrow(wq),ncol=Nit)
-                  
-                  D=c()
-                  
-                  
-                  Dens<-Densevalm11(t_old,RC)
-                  p_old=Dens$p
-                  x_old=Dens$x
-                  yp_old=Dens$yp
-                  ypo_old=Dens$ypo
-                  D_old=Dens$D
-                  
-                  for(i in 1:Nit){
-                      
-                      t_new=t_old+solve(t(LH),as.matrix(rnorm(2,0,1)))
-                      
-                      Densnew<-Densevalm11(t_new,RC)
-                      p_new=Densnew$p
-                      x_new=Densnew$x
-                      yp_new=Densnew$yp
-                      ypo_new=Densnew$ypo
-                      D_new=Densnew$D
-                      
-                      logR=p_new-p_old
-                      if (logR>log(runif(1))){
-                          t_old=t_new
-                          x_old=x_new
-                          p_old=p_new
-                          yp_old=yp_new
-                          ypo_old=ypo_new
-                          D_old=D_new
-                      }
-                      
-                      t[,i]=rbind(t_m,x_old)
-                      yp[,i]=yp_old
-                      ypo[,i]=ypo_old
-                      
-                      D[i]=D_old
-                  }
-                  
-                  if(j==1){
-                      t1=t
-                      yp1=yp
-                      ypo1=ypo
-                      D1=D
-                  } else if(j==2){
-                      t2=t
-                      yp2=yp
-                      ypo2=ypo
-                      D2=D
-                  } else if(j==3){
-                      t3=t
-                      yp3=yp
-                      ypo3=ypo
-                      D3=D
-                  } else if(j==4){
-                      t4=t
-                      yp4=yp
-                      ypo4=ypo
-                      D4=D
-                  }
-                  
-              }
-              
-              
-              Dhat=-2*sum(log(dlnorm(exp(RC$y),X_m%*%mu,sqrt(exp(t_m[2])))))
-              seq=seq(2000,20000,5)
-              Davg=mean(c(D1[seq],D2[seq],D3[seq],D4[seq]))
-              pd=Davg-Dhat
-              DIC=Dhat+2*pd
-              B=1/(mean(0.5*c(D1[seq],D2[seq],D3[seq],D4[seq])))
-              
-              #c(Dhat, Davg, DIC, pd, B) #afhverju thessi vigur?
-             
-              
-             return(list("RC"=RC,"ypo1"=ypo1,"ypo2"=ypo2,"ypo3"=ypo3,"ypo4"=ypo4,"l_m"=l_m,"t_m"=t_m,"qvdata"=qvdata,"fit"=X_m%*%mu, "mu"=mu, "varappr"=varappr))
-              ## MODEL 1 ## End
-          })
-      }
-    })
-
     
-    model1fast <-reactive({
+    model1 <-reactive({
         if(!is.null(data())){
             wq=data()$wq
             qvdata=data()$qvdata
@@ -224,7 +86,7 @@ model1<-reactive({
     })
         }
     })
-    plotratingcurveslow <- eventReactive(input$go,{
+    plotratingcurve2 <- eventReactive(input$go,{
         plotlist=model1()
         rclog=NULL
         rcraun=NULL
@@ -306,8 +168,8 @@ model1<-reactive({
         
         
     })
-    plotratingcurve <- eventReactive(input$go,{
-        plotlist=model1fast()
+    plotratingcurve1 <- eventReactive(input$go,{
+        plotlist=model1()
         rclog=NULL
         rcraun=NULL
         rcleiflog=NULL
@@ -322,7 +184,6 @@ model1<-reactive({
             
             simdata=data.frame(fit=as.matrix(seq(min(data$fit),max(data$fit),length.out=1000)))
             simdata$fitl_m = as.matrix(seq(min(data$l_m),max(data$l_m),length.out=1000))
-#           simdata$Wfit=seq(min(log(data$W)),max(log(data$W)),length.out=1000)
             c_hat=min(data$W)-exp(plotlist$t_m[1,])
             simdata$Wfit = exp(simdata$fitl_m) + c_hat
             data$upper=plotlist$RC$confinterval[,2]
@@ -382,13 +243,13 @@ model1<-reactive({
 })
     
     output$callreactive <- renderPrint({
-        #plotlist=model1()
-        plotlist2=model1fast()
+        plotlist=model1()
+        
     })
     
     output$plots <- renderUI({
-        if(length(plotratingcurve())!=0){
-            plot_output_list <- lapply(1:(length(plotratingcurve())-1), function(i) {
+        if(length(plotratingcurve1())!=0){
+            plot_output_list <- lapply(1:(length(plotratingcurve1())-1), function(i) {
                 plotname=paste("plot", i, sep="")
                 plotOutput(plotname)
             })
@@ -397,36 +258,25 @@ model1<-reactive({
         }
     })
     
-    
-    
-    #     for(i in 1:5){
-    #             my_i=i
-    #             local({
-    #                 name=paste("plot", my_i, sep="")
-    #                 output[[name]]<-renderPlot({
-    #                     plotratingcurve()[[i]]    
-    #                 })
-    #             })
-    #     }
     output$plot1<-renderPlot({
-        if(length(plotratingcurve())!=0)
-            plotratingcurve()[[1]]
+        if(length(plotratingcurve1())!=0)
+            plotratingcurve1()[[1]]
     },height=400,width=600)
     output$plot2<-renderPlot({
-        if(length(plotratingcurve()) >= 2)
-            plotratingcurve()[[2]]   
+        if(length(plotratingcurve1()) >= 2)
+            plotratingcurve1()[[2]]   
     },height=400,width=600)
     output$plot3<-renderPlot({
-        if(length(plotratingcurve())>=3)
-            plotratingcurve()[[3]]    
+        if(length(plotratingcurve1())>=3)
+            plotratingcurve1()[[3]]    
     },height=400,width=600)
     output$plot4<-renderPlot({
-        if(length(plotratingcurve())>=4)
-            plotratingcurve()[[4]]     
+        if(length(plotratingcurve1())>=4)
+            plotratingcurve1()[[4]]     
     },height=400,width=600)
     output$tafla <- renderGvis({
-    #       if(!is.null(plotratingcurve()$tafla))
-                table=as.data.frame(plotratingcurve()$tafla)
+    #       if(!is.null(plotratingcurve1()$tafla))
+                table=as.data.frame(plotratingcurve1()$tafla)
                 gvisTable(table,options=list(
                     page='enable',
                     pageSize=30,
@@ -453,7 +303,7 @@ model1<-reactive({
             library(rmarkdown)
             out <- render('myreport.Rmd',pdf_document())
             file.rename(out, file)
-        },
+        }
        
        
     )
@@ -462,19 +312,6 @@ model1<-reactive({
 })
 
 
-# content <- function(file) {
-#     src <- normalizePath('myreport.Rnw')
-#     
-#     # temporarily switch to the temp dir, in case you do not have write
-#     #permission to the current working directory
-#     owd <- setwd(tempdir())
-#     on.exit(setwd(owd))
-#     file.copy(src, 'myreport.Rnw')
-#     
-#     
-#     out <- knit2pdf('myreport.Rnw', clean=TRUE)
-#     file.rename(out, file)
-# },
-# contentType='application/pdf'
+
 
 
