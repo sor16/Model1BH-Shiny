f9730a78fc1a4fb4ec8a26ba3ae853814a6ca850
sor16 <solviro@gmail.com>
2015-06-23 15:43:56 +0000

    Model 2 added along with plotratingcurve2

--- server.R
+++ server.R
@@ -15,8 +15,6 @@ shinyServer(function(input, output) {
         else if (inFile$type =="text/plain"){
             qvdata=read.table(inFile$datapath,skip=3,sep="|",dec=",")
             qvdata=qvdata[,c(2:4,7)]
-            #qvdata=data.frame(lapply(qvdata, as.character), stringsAsFactors=FALSE)
-            #qvdata[,3:4]=apply(qvdata[,3:4],2, function(x) as.numeric(gsub(",",".",x)))
             qvdata[,3:4]=qvdata[,4:3]
             names(qvdata)=c("Date","Time","W","Q")
             qvdata$Time=as.character(qvdata$Time)
@@ -25,7 +23,7 @@ shinyServer(function(input, output) {
             
         }
         else if(inFile$type=="text/csv"){
-            qvdata=read.table(inFile$datapath,skip=16)
+            qvdata=read.csv(inFile$datapath,skip=16)
             qvdata=qvdata[,c(1:3,6)]
             qvdata=data.frame(lapply(qvdata, as.character), stringsAsFactors=FALSE)
             qvdata[,3:4]=apply(qvdata[,c(3,4)],2, function(x) as.numeric(gsub(",",".",x)))
@@ -89,16 +87,151 @@ shinyServer(function(input, output) {
     
     model2 <- reactive({
         if(!is.null(data())){
-        wq=dara()$wq
+        wq=data()$wq
         qvdata=data()$qvdata
             withProgress(message = 'Making plot', value = 0, {
+                Nit=20000
+                
+                RC=priors(input$select)
+                
+                RC$nugget=10^-8
+                RC$mu_sb=0.5
+                RC$mu_pb=0.5
+                RC$tau_pb2=0.25^2
+                RC$s=3
+                RC$v=5
+                
+                RC$y=rbind(as.matrix(log(wq[,2])),0)
+                RC$w=as.matrix(0.01*wq[,1])
+                RC$w_tild=RC$w-min(RC$w)
+                
+                Adist1 <- Adist(RC$w)
+                RC$A=Adist1$A
+                RC$dist=Adist1$dist
+                RC$n=Adist1$n
+                RC$N=Adist1$N
+                
+                RC$P=diag(nrow=5,ncol=5,6)-matrix(nrow=5,ncol=5,1)
+                RC$Sig_ab= rbind(c(RC$sig_a^2, RC$p_ab*RC$sig_a*RC$sig_b), c(RC$p_ab*RC$sig_a*RC$sig_b, RC$sig_b^2))
+                RC$mu_x=as.matrix(c(RC$mu_a,RC$mu_b, rep(0,RC$n))) #Setja i RC
+                
+                RC$B=B_splines(t(RC$w_tild)/RC$w_tild[length(RC$w_tild)])
+                RC$Z=cbind(t(rep(0,2)),t(rep(1,RC$n)))
+                
+                RC$m1=matrix(0,nrow=2,ncol=RC$n)
+                RC$m2=matrix(0,nrow=RC$n,ncol=2)
+                
+                Dens = function(th) {-Densevalm22fast(th,RC)$p}
+                Densmin=optim(par=theta.init,Dens,method="BFGS",hessian=TRUE)
+                t_m =Densmin$par
+                H=Densmin$hessian
+                l=log(RC$w_tild+exp(t_m[1])) #as.matrix
+                varr_m=exp(RC$B%*%lambda)
+                
+                t1=matrix(0,9,Nit)
+                t2=matrix(0,9,Nit)
+                t3=matrix(0,9,Nit)
+                t4=matrix(0,9,Nit)
+                
+                xsiz=max(dim(mu))
+                
+                x1=matrix(0,xsiz,Nit)
+                x2=matrix(0,xsiz,Nit)
+                x3=matrix(0,xsiz,Nit)
+                x4=matrix(0,xsiz,Nit)
+                
+                
+                for(j in 1:4){
+                    t_old=t_m
+                    t=matrix(0,nrow=9,ncol=Nit)
+                    x=matrix(0,nrow=xsiz,ncol=Nit)
+                    yp=matrix(0,nrow=RC$N,ncol=Nit)
+                    ypo=matrix(0,nrow=RC$N,ncol=Nit)
+                    varr=matrix(0,nrow=RC$N,ncol=Nit)
+                    D=matrix(0,nrow=1,ncol=Nit)
+                    
+                    
+                    
+                    Dens<-Densevalm22fast(t_old,RC)
+                    p_old=Dens$p
+                    x_old=Dens$x
+                    yp_old=Dens$yp
+                    ypo_old=Dens$ypo
+                    D_old=Dens$D
+                    varr_old=Dens$varr
+                    
+                    for(i in 1:Nit){
+                        t_new=t_old+solve(t(LH),as.matrix(rnorm(9,0,1)))
+                        
+                        Densnew<-Densevalm22fast(t_new,RC)
+                        p_new=Densnew$p
+                        x_new=Densnew$x
+                        yp_new=Densnew$yp
+                        ypo_new=Densnew$ypo
+                        D_new=Densnew$D
+                        varr_new=Densnew$varr
+                        
+                        logR=p_new-p_old
+                        
+                        if (logR>log(runif(1))){
+                            t_old=t_new
+                            x_old=x_new
+                            p_old=p_new
+                            yp_old=yp_new
+                            ypo_old=ypo_new
+                            D_old=D_new
+                            varr_old=varr_new
+                        }
+                        
+                        t[,i]=t_old
+                        yp[,i]=yp_old
+                        ypo[,i]=ypo_old
+                        
+                        D[1,i]=D_old
+                        varr[,i]=varr_old
+                    }
+                    
+                    if(j==1){
+                        t1=t
+                        yp1=yp
+                        ypo1=ypo
+                        D1=D
+                        varr1=varr
+                    } else if(j==2){
+                        t2=t
+                        yp2=yp
+                        ypo2=ypo
+                        D2=D
+                        varr2=varr
+                    } else if(j==3){
+                        t3=t
+                        yp3=yp
+                        ypo3=ypo
+                        D3=D
+                        varr3=varr
+                    } else if(j==4){
+                        t4=t
+                        yp4=yp
+                        ypo4=ypo
+                        D4=D
+                        varr4=varr
+                    }
+                }
+                
             
             }
         }
+        seq=seq(2000,20000,5)
+        quantypo1=ypo1[,seq]
+        quantypo2=ypo2[,seq]
+        quantypo3=ypo3[,seq]
+        quantypo4=ypo4[,seq]
+        quantmatrix=t(cbind(quantypo1,quantypo2,quantypo3,quantypo4))
+        return(list("RC"=RC,"l"=l,"varr_m"=varr_m,"qvdata"=qvdata,"quantmatrix"=quantmatrix ))
     })
     
     plotratingcurve2 <- eventReactive(input$go,{
-        plotlist=model1()
+        plotlist=model2()
         rclog=NULL
         rcraun=NULL
         rcleiflog=NULL
@@ -107,24 +240,14 @@ shinyServer(function(input, output) {
         outputlist=list()
         if(!is.null(plotlist$qvdata)) {
             
-            data=data.frame(W=plotlist$RC$w,Q=plotlist$RC$y)
-            data$l_m=plotlist$l_m
-            data$fit=plotlist$fit
-            
-            #simdata=data.frame(fit=as.matrix(seq(min(data$fit),max(data$fit),length.out=1000)))
-            #simdata$fitl_m = as.matrix(seq(min(data$l_m),max(data$l_m),length.out=1000))
-            #simdata$Wfit=seq(min(log(data$W)),max(log(data$W)),length.out=1000)
+            data=data.frame(W=plotlist$RC$w,Q=plotlist$RC$y[1:plotlist$RC$N,])
+            data$l_m=plotlist$l
             data$c_hat=rep(min(data$W)-exp(plotlist$t_m[1,]),length(data$l_m))
-            #simdata$Wfit = exp(simdata$fitl_m) + c_hat
-            seq=seq(2000,20000,5)
-            quantypo1=plotlist$ypo1[,seq]
-            quantypo2=plotlist$ypo2[,seq]
-            quantypo3=plotlist$ypo3[,seq]
-            quantypo4=plotlist$ypo4[,seq]
-            quantmatrix=t(cbind(quantypo1,quantypo2,quantypo3,quantypo4))
-            prctile=t(apply(quantmatrix, 2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE))
+            
+            prctile=t(apply(plotlist$quantmatrix, 2, quantile, probs = c(0.025,0.5, 0.975),  na.rm = TRUE))
             data$lower=prctile[,1]
-            data$upper=prctile[,2]
+            data$fit=prctile[,2]
+            data$upper=prctile[,3]
             
             
             if ("raun" %in% input$checkbox){
@@ -154,7 +277,7 @@ shinyServer(function(input, output) {
                 outputlist$rcleifraun=rcleifraun
             } 
             if("leiflog" %in% input$checkbox){
-                data$residlog=(data$Q-data$fit)/sqrt(exp(plotlist$t_m[2,]))
+                data$residlog=(data$Q-data$fit)/sqrt(exp(plotlist$varr_m))
                 rcleiflog=ggplot(data)+geom_point(aes(l_m,residlog),color="red")+theme_bw()+geom_abline(intercept = 0, slope = 0)+
                     geom_abline(intercept = 2, slope = 0,linetype="dashed")+geom_abline(intercept = -2, slope = 0,linetype="dashed")+ylim(-4,4)+
                     ylab(expression(epsilon[i]))+ggtitle("Residual plot (log scale)")+xlab(expression(log(W-hat(c))))+
